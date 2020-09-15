b4abe09cfb52ecbdeec8ff7f6f950d18556cc2c0
Hzcheng1012 <huangzucheng>
2020-09-15 11:41:01 +0800

    初始化项目

--- magno-admin/src/main/java/com/magno/AdminApplication.java
+++ magno-admin/src/main/java/com/magno/AdminApplication.java
@@ -4,17 +4,16 @@ package com.magno;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 
-
-
-//@EnableDiscoveryClient
 //@EnableTransactionManagement(order = 2)	//设置事务执行顺序(需要在切换数据源之后，否则只走主库)
 //@ComponentScan("com.crets.*")
 //@MapperScan("com.crets.**.model.*")
 //@EnableFeignClients(basePackages="com.crets")
 //@EnableSwagger2
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AdminApplication {
 	public static void main(String[] args) {
